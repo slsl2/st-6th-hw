bad14b5299b96f392fd9f8d4bc3f8aa11adc98fb
slsl2 <kimminji7153@gmail.com>
2024-05-30 15:15:42 +0900

    ex4 제출

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,1 +1,27 @@
 // TODO: 이곳에 커스텀훅 작성하세요.
+import { useState, useEffect } from "react";
+export default function useFetch(url) {
+  const [title, setTitle] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        console.log("response:", response);
+        if (!response.ok) {
+          throw new Error("Network 오류");
+        }
+        const result = await response.json();
+        setTitle(result.title);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+  return { title, loading, error };
+}
